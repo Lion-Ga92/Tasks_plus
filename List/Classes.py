2a64f158a5fc2e9c08b3adb2b0f023a77a7aa053
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-06-15 01:34:55 -0700

    Uploading my tasks_list app to github

---
+++ List/Classes.py
@@ -0,0 +1,61 @@
+import datetime
+
+class tasks(object):
+
+    def __init__(self, task1, task2, task3):
+        self.task1 = task1
+        self.task2 = task2
+        self.task3 = task3
+
+    def list_tasks(self):
+        print("1: " + self.task1)
+        print("2: " + self.task2)
+        print("3: " + self.task3)
+
+
+
+    def check_for_task(self):
+        completed_tasks = ""
+        completed_tasks += input("Type in name and date: ")
+        completed_tasks += "\n"
+        print("Did you", self.task1, "?")
+        task_one_check = input("Yes or no: ")
+        if task_one_check == "yes":
+            print("You", self.task1, "!")
+            completed_tasks += "You " + self.task1 + "!" + "\n"
+        elif task_one_check == "no":
+            print("You still need to", self.task1)
+            completed_tasks += "You still need to " + self.task1 + "\n"
+        else:
+            print("Not valid, type 'yes' or 'no' please")
+
+        print("Did you", self.task2, "?")
+        task_two_check = input("'Yes' or 'no': ")
+        if task_two_check == "yes":
+            print("You", self.task2, "!")
+            completed_tasks += "You, " +self.task2 + "!" + "\n"
+        elif task_two_check == "no":
+            print("You still need to", self.task2)
+            completed_tasks += "You still need to, " + self.task2 + "\n"
+        else:
+            print("Not valid, type 'yes' or 'no' please")
+
+        print("Did you", self.task3, "?")
+        task_three_check = input("'Yes' or 'no': ")
+        if task_three_check == "yes":
+            print("You", self.task3, "!")
+            completed_tasks += "You, " + self.task3 + "!" + "\n"
+        elif task_three_check == "no":
+            print("You still need to", self.task3)
+            completed_tasks += "You still need to, " + self.task3 + "\n"
+        else:
+            print("Response not valid")
+
+        completed_tasks += "\n"
+
+        task_file = open("task_list.txt", "a+")
+        task_file.write(completed_tasks)
+
+
+
+
---
+++ List/main.py
@@ -0,0 +1,18 @@
+from List.Classes import tasks
+
+day_one = tasks("Cut corn", "Peel corn", "Wash pots")
+day_two = tasks("Take out thrash", "Thaw tamales", "Cook")
+
+print("Welcome to your amazing Task tracker, input your answers and in lowercase please")
+print("""our tasks are divided in two groups. The first group are the work day tasks, and the second group are the 
+non work day ones. """)
+print("\n")
+print("Work days")
+day_one.list_tasks()
+print("\n")
+print("Non-work days")
+day_two.list_tasks()
+
+day_one.check_for_task()
+print("\n")
+day_two.check_for_task()
